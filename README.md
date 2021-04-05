4be93436311e3838868368d713f4412af777321f
huhuhang <huhuhang@gmail.com>
2021-04-05 11:42:27 +0800

    cleanup

---
+++ README.md
@@ -0,0 +1,9 @@
+## Farbox 2 支持自动同步模板
+
+### 初始化配置
+
+点击 `Use this template` 按钮基于本模板仓库创建自己的 **私有仓库**。务必使用私有仓库，否则可能会暴露你的私钥地址。当然，如果你需要公开自己的博客内容，可以借助 [Actions secrets](/settings/secrets/actions) 功能保护自己的私钥。
+
+
+
+
---
+++ sync_to_server.py
@@ -0,0 +1,18 @@
+# coding: utf8
+import os
+from farbox_bucket.client.sync import sync_to_farbox
+
+# 如果 DEBUG 设定是 yes，会在日志中输出同步的记录
+os.environ['DEBUG'] = 'yes'
+
+private_key = """
+填写你的私钥
+"""
+
+root = "./"
+
+# 节点
+node = "填写服务器IP"
+
+# 开始同步
+sync_to_farbox(node=node, root=root, private_key=private_key)
